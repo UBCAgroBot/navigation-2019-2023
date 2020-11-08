95245d9d71dd9d390949d6c120e8ad2f71d18c54
davidw0311 <=>
2020-11-08 10:02:18 -0800

    added workspace

---
+++ agrobot_ws/src/run_agrobot.sh
@@ -0,0 +1,4 @@
+source ~/agrobot_ws/devel/setup.bash
+roslaunch agrobot my_launch.launch
+# gnome-terminal -e rosrun teleop_twist_keyboard teleop_twist_keyboard.py
+# gnome-terminal -e rosrun rqt_image_view rqt_image_view
