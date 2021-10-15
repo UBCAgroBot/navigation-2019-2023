0c2c72670c2eed560ca24566da1f1e3d5be412de
Asad Dhorajiwala <asadrehandhorajiwala@gmail.com>
2021-10-14 23:43:31 -0700

    Renaming and formatting

--- helper_scripts/extract_frames.py
+++ helper_scripts/extract_frames.py
@@ -3,23 +3,23 @@ import os
 import cv2 as cv
 
 path = '../videos/'
-file = 'sim_trimmed'
+file = 'sim_cut'
 file_type = '.mp4'
 
-os.makedirs('../keyframes/' + file)
+os.makedirs(f'../keyframes/{file}')
 
 vid = cv.VideoCapture(path + file + file_type)
 success, frame = vid.read()
 count = 0
 
-h, w, l = frame.shape
-out = cv.VideoWriter(path + file + '_keyframes' + file_type, cv.VideoWriter_fourcc(*'mp4v'), 1, (w, h))
+h, w, d = frame.shape
+out = cv.VideoWriter(path + f'{file}_key' + file_type, cv.VideoWriter_fourcc(*'mp4v'), 1, (w, h))
 
 while success:
     if count % 30 == 0:
-        frame = cv.line(frame, (w//2, 0), (w//2, h), (0, 0, 255), thickness=2)
+        frame = cv.line(frame, (w // 2, 0), (w // 2, h), (0, 0, 255), thickness=2)
         out.write(frame)
-        cv.imwrite('../keyframes/' + file + '/frame%d.jpg' % count, frame)
+        cv.imwrite(f'../keyframes/{file}/frame%d.jpg' % count, frame)
         print('frame ', count)
     success, frame = vid.read()
     count += 1
