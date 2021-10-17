3b4a323ba7766800fef897632d25d7b7e7f0f4b3
davidw0311 <davidw0311@gmail.com>
2021-10-17 15:55:40 -0700

    added farm videos, added tk for opening videos in pipenv

--- helper_scripts/play_video.py
+++ helper_scripts/play_video.py
@@ -1,21 +1,25 @@
 import cv2
-video = '../videos/sunflower.mp4'
+from tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
 
 if __name__ == '__main__':
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    video_file = askopenfilename(initialdir='../videos') # show an "Open" dialog box and return the path to the selected file
+    
+    vid = cv2.VideoCapture(video_file)
 
-    vid = cv2.VideoCapture(video)
-
+    video_name = video_file.split('/')[-1].split('.')[0]
     if (vid.isOpened() == False):
         print("Error Opening Video File")
 
     while (vid.isOpened()):
         ret, frame = vid.read()
-        print(frame.shape)
+        
         if ret == False:
             print("No More Frames Remaining")
             break
-
-        cv2.imshow("video", frame)
+        print(frame.shape)
+        cv2.imshow(video_name, frame)
         key = cv2.waitKey(25)
 
         #### Exit if Esc key is pressed
@@ -23,7 +27,7 @@ if __name__ == '__main__':
             break
         #### save frame if 's' key is pressed
         if key == ord('s'):
-            cv2.imwrite('saved_frame.jpg', frame)
+            cv2.imwrite('%s.jpg'%video_name, frame)
             print('current frame saved')
             cv2.waitKey(2000)
 
