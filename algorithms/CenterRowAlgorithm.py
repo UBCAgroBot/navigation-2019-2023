b3aacb3cebbcc2d45014da5c65fafd50b98713d9
Asad Dhorajiwala <asadrehandhorajiwala@gmail.com>
2021-11-21 12:22:29 -0800

    Multiple changes
    - removed max_contour_area since it was making the algo worse
    - added docstrings for functions
    - minor formatting and renaming
    - added get_center_row_coords and get_angle_to_minimize

--- algorithms/CenterRowAlgorithm.py
+++ algorithms/CenterRowAlgorithm.py
@@ -3,14 +3,15 @@ import math
 import cv2 as cv
 import numpy as np
 
-# from helper_scripts import hsv_threshold
 from algorithms.utils import Lines
 
 
 class CenterRowAlgorithm:
 
-    # constructor -- assigns the following parameters
     def __init__(self, config):
+        """Sets center row algorithm configurations\n
+        :param config: config params
+        """
 
         # hsv mask
         self.low_green = np.array(config.lower_hsv_threshold)
@@ -25,7 +26,6 @@ class CenterRowAlgorithm:
         # contour parameters
         self.contour_color = list(map(int, config.contour_color.split(',')))
         self.min_contour_area = config.min_contour_area
-        self.max_contour_area = config.max_contour_area
 
         # Canny edge parameters
         self.canny_low_threshold = config.canny_low_threshold
@@ -38,61 +38,47 @@ class CenterRowAlgorithm:
         # contours
         self.contours = []
 
+        # center contour
+        self.center = None
+        self.center_angle = 0
+
     def processFrame(self, frame, show=True):
-        # This function uses contouring to create contours around each crop row
-        # and uses these contours to find centroid lines, and the correspond row vanishing point
-        # This function takes in the current frame (mat) as a parameter, and returns the
-        # vanishing point (tuple)
-
-        black_frame = np.uint8(np.zeros((720, 1280, 3)))
-
-        # h, w, c = frame.shape
-        # frame = frame[h // 2:h, 0:w]
-
+        """Uses contouring to create contours around each crop row and uses these contours to find centroid lines,
+        row vanishing point, a center contour and the angle between the center contour and vanishing point\n
+        :param frame: current frame (mat)
+        :param show: show/hide frames on screen for debugging
+        :type show: bool
+        :return: processed frame (mat), vanishing point (tuple)
+        """
         mask = self.create_binary_mask(frame)
 
         # Perform canny edge detection
-        edges = cv.Canny(mask, self.canny_low_threshold, self.canny_high_threshold)
-        blurred_edges = self.gaussian_blur(edges)
+        # edges = cv.Canny(mask, self.canny_low_threshold, self.canny_high_threshold)
+        # blurred_edges = self.gaussian_blur(edges)
 
-        cnt, contour_frame = self.get_contours(mask)
-        cv.drawContours(black_frame, cnt, -1, self.contour_color, 3)
+        # current frame is a 720 x 1280 black frame
+        black_frame = np.uint8(np.zeros((720, 1280, 3)))
+
+        contours, contour_frame = self.get_contours(mask)
+        cv.drawContours(black_frame, contours, -1, self.contour_color, 3)
         # fillPoly fills in the polygons in the frame
-        cv.fillPoly(black_frame, pts=cnt, color=self.contour_color)
-        lines, slopes, ellipse_frame = self.ellipse_slopes(cnt, black_frame)
+        cv.fillPoly(black_frame, pts=contours, color=self.contour_color)
+        lines, slopes, ellipse_frame = self.ellipse_slopes(contours, black_frame)
         Lines.drawLinesOnFrame(lines, black_frame)
         intersections, points = Lines.getIntersections(lines)
         vanishing_point = Lines.drawVanishingPoint(ellipse_frame, points)
 
-        # print("vanishing point", vanishing_point)
         if vanishing_point:
-            min_contour, angle = self.get_min_angle(vanishing_point)
-            cv.ellipse(black_frame, min_contour, (0, 255, 0), 2)
+            center_contour, angle = self.find_center_contour(vanishing_point)
+            cv.ellipse(black_frame, center_contour, (0, 255, 0), 2)
 
         return black_frame, vanishing_point
 
-    def get_min_angle(self, vanishing_point):
-
-        min_del_x = math.inf
-        min_angle = math.inf
-        min_contour = None
-
-        for contour in self.contours:
-            center = contour[0]
-            del_x = vanishing_point[0] - center[0]
-            if abs(del_x) < min_del_x:
-                del_y = vanishing_point[1] - 400 - center[1]
-                angle = math.atan(del_x / del_y)
-                min_angle = angle
-                min_contour = contour
-                min_del_x = abs(del_x)
-
-        return min_contour, min_angle
-
     def create_binary_mask(self, frame):
-        # Current frame is input as a parameter
-        # The function uses HSV filtering with the specified low_green to high_green HSV range
-        # to binarize the image and returns the binary frame
+        """
+        :param frame: current frame
+        :return: binary mask of frame made using self.low_green and self.high_green as the HSV range
+        """
 
         # Run averaging filter to blur the frame
         kernel = np.ones((5, 5), np.float32) / 25
@@ -100,34 +86,44 @@ class CenterRowAlgorithm:
 
         # Convert to hsv format to allow for easier colour filtering
         hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
         # Filter image and allow only shades of green to pass
         mask = cv.inRange(hsv, self.low_green, self.high_green)
 
         return mask
 
-    def gaussian_blur(self, frame):
-        # Current frame is input as a parameter
-        # This function applies gaussian blurring to the frame
-        # And returns the resulting blurred frame
-        mask = cv.GaussianBlur(frame, self.gauss_kernel_size, self.sigmaX)
-        return mask
+    def get_contours(self, binary_mask):
+        """ Uses the cv.findContours to find the contours in binary_mask
+        (creates closed shapes around connected pixels)\n
+        :param binary_mask: binary mask of current frame
+        :return: list of contours, blank frame with contours filled in
+        """
+        frame = np.zeros((self.HEIGHT, self.WIDTH, 3))
+        ret, thresh = cv.threshold(binary_mask, 0, 254, 0)
+        contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        cv.drawContours(frame, contours, -1, (0, 255, 0), 2)
+        cv.fillPoly(frame, pts=contours, color=(0, 255, 0))
 
-    def ellipse_slopes(self, cnt, black_frame):
-        # Takes in the list of contours on the frame and the frame with contours (black frame) as parameters
-        # This function draws ellipses around each contour on black_frame using the fitEllipse function
-        # Uses the fitline function with the list of contours to create a set of lines
-        # Returns an array of lines, an array of slopes and an updated frame with ellipses and lines
+        return contours, frame
+
+    def ellipse_slopes(self, contours, black_frame):
+        """Draws ellipses around each contour on black_frame using cv.fitEllipse,
+        Uses cv.fitline with the list of contours to create a set of lines\n
+        :param contours: list of contours on the current frame
+        :param black_frame: black frame with contours (mat)
+        :return: lines (array), slops (array), updated frame with ellipses and lines
+        """
         slopes = []
         lines = []
         self.contours = []
-        for cnt in cnt:
-            if self.min_contour_area < cv.contourArea(cnt) < self.max_contour_area:
+
+        for cnt in contours:
+            if self.min_contour_area < cv.contourArea(cnt):
                 rect = cv.minAreaRect(cnt)
                 box = cv.boxPoints(rect)
                 box = np.int0(box)
                 black_frame = cv.drawContours(black_frame, [box], 0, (255, 255, 255), 2)
                 ellipse = cv.fitEllipse(cnt)
-                # print("center", ellipse[0])
                 self.contours.append(ellipse)
                 cv.ellipse(black_frame, ellipse, (255, 255, 255), 2)
                 rows, cols = black_frame.shape[:2]
@@ -145,17 +141,48 @@ class CenterRowAlgorithm:
 
         return lines, slopes, black_frame
 
-    def get_contours(self, binary_mask):
-        # Takes in a binary image as a parameter
-        # Uses the cv findContours function to find the contours (creates closed shapes around connected pixels) in the image
-        # returns a list of contours and blank frame with contours filled in
-        frame = np.zeros((self.HEIGHT, self.WIDTH, 3))
-        ret, thresh = cv.threshold(binary_mask, 0, 254, 0)
-        contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        cnt = contours
-        cv.drawContours(frame, cnt, -1, (0, 255, 0), 2)
-        cv.fillPoly(frame, pts=cnt, color=(0, 255, 0))
+    def find_center_contour(self, vanishing_point):
+        """Finds center most contour by finding the contour which has the closes x-value to the vanishing point,
+        calculates angle between center contour and vanishing point\n
+        :param vanishing_point: vanishing point
+        :type vanishing_point: (int, int)
+        :return: center contour, angle  between center contour and vanishing point
+        """
+        min_del_x = math.inf
+        min_angle = math.inf
+        min_contour = None
 
-        return cnt, frame
+        for contour in self.contours:
+            center = contour[0]
+            del_x = vanishing_point[0] - center[0]
+            if abs(del_x) < min_del_x:
+                del_y = vanishing_point[1] - 400 - center[1]
+                min_angle = math.atan(del_x / del_y)
+                min_contour = contour
+                min_del_x = abs(del_x)
 
-    
+        self.center = min_contour
+        self.center_angle = min_angle
+
+        return min_contour, min_angle
+
+    def get_center_row_coords(self):
+        """
+        :return: center coordinates of center row contour (x, y) for most recently processed frame.
+        """
+        # x-value, self.center[0][0], is relevant for minimization, y-value is not that important
+        return self.center[0]
+
+    def get_angle_to_minimize(self):
+        """
+        :return: angle to minimize for most recently processed frame in Radians.
+        It is the angle between the vanishing point and the center of the center row contour
+        """
+        return self.center_angle
+
+    def gaussian_blur(self, frame):
+        """Applies gaussian blurring to the frame\n
+        :param frame: current frame
+        :return: frame with gaussian blurring
+        """
+        return cv.GaussianBlur(frame, self.gauss_kernel_size, self.sigmaX)
