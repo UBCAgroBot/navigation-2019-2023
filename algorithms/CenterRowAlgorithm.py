02253ac31c00d1b295385493e37af0ee9b823419
Rithin Kumar <rithinkumar13@gmail.com>
2022-10-21 20:08:27 -0700

    simple pep failure

--- algorithms/CenterRowAlgorithm.py
+++ algorithms/CenterRowAlgorithm.py
@@ -43,7 +43,7 @@ class CenterRowAlgorithm(Algorithm):
         # initialize super
         super().__init__(config)
 
-    def process_frame(self, frame, show):
+    def processFrame(self, frame, show):
         """Uses contouring to create contours around each crop row and uses these contours to find centroid lines,
         row vanishing point, a center contour and the angle between the center contour and vanishing point\n
         :param frame: current frame (mat)
