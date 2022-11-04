eeea3dbb0c141f04115fa6fcef5061067939270b
zoeyzhao57 <zoeyzhao57@gmail.com>
2022-11-04 11:37:36 -0700

    downwards center row method

--- algorithms/MiniContoursDownwards.py
+++ algorithms/MiniContoursDownwards.py
@@ -1,151 +1,180 @@
-import cv2
-import numpy as np
 import math
-from .MiniContoursAlgorithm import MiniContoursAlgorithm
 
-class MiniContoursDownwards(MiniContoursAlgorithm):
-    
+import cv2 as cv
+import numpy as np
+from algorithms.Algorithm import Algorithm
+
+from algorithms.utils import Lines
+
+
+class MiniContoursDownwards(Algorithm):
+
     def __init__(self, config):
-        MiniContoursAlgorithm.__init__(self,config)
-        self.speed_up = 5
-        self.frame_counter = 0
-        self.last_valid_frame = []
-        self.end_of_row_cut_off = 0.6
+        """
+        Sets center row algorithm configurations
+        :param config: config params
+        """
 
-    def get_center_hough_lines(self, frame, show, num_strips=60, lines_max=30, threshold=4, min_rho=0, max_rho=1000, rho_step=1, min_theta=-math.pi/4, max_theta=math.pi/4, theta_step=math.pi/180,
-                            draw_points=False):
-        # frame: BGR frame 
-        # num_strips: number of strips for centroid calculation
-        # other parameters for HoughLinesPointSet
-        # returns: 
-        # frame: original frame with hough lines drawn on
-        # lines: list of [[votes, rho, theta]] of all lines
-        # point_lines: list of [votes, pt1, pt2] of all lines        
-        
-        mask = cv2.inRange(cv2.cvtColor(frame, cv2.COLOR_BGR2HSV), self.low_green, self.high_green)
-        mask = cv2.medianBlur(mask, 9)
-        kernel = cv2.getStructuringElement(shape=cv2.MORPH_RECT, ksize=(3,3))
-        mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, kernel, iterations = 3)
-        centroids = self.getCentroids(mask, num_strips=num_strips)
-        
-        points = np.zeros(mask.shape, dtype=np.uint8)
-        points = []
+        # masking range for green
+        self.LOW_GREEN = np.array(config.lower_hsv_threshold)
+        self.HIGH_GREEN = np.array(config.upper_hsv_threshold)
 
-        height, width = frame.shape[0], frame.shape[1]
-        split_factor = 0.25
-        if draw_points:
-            cv2.line(frame,(int(width*split_factor), 0), (int(width*split_factor), height), self.color3, thickness=2)
-            cv2.line(frame,(int(width*(1-split_factor)), 0), (int(width*(1-split_factor)), height), self.color3, thickness=2)
-        for i, strip_centroid in enumerate(centroids):
-            for centroid in strip_centroid:
-                x,y = centroid[0], centroid[1]
-                if x > width*split_factor and x < width*(1-split_factor):
-                    # vertically split the points
-                    if draw_points:
-                        cv2.circle(frame, (int(centroid[0]), int(centroid[1])), 3, self.color1, -1) 
-                        cv2.circle(mask, (int(centroid[0]), int(centroid[1])), 3, self.color1, -1)
-                    points.append([int(centroid[0]), int(centroid[1])])
-                else:
-                    if draw_points:
-                        cv2.circle(frame, (int(centroid[0]), int(centroid[1])), 3, self.color3, -1)     
+        # filtering parameters
+        self.averaging_kernel_size = config.averaging_kernel_size
+        self.gauss_kernel_size = list(map(int, config.gauss_kernel_size.split(',')))
+        self.dilate_kernel_size = config.dilate_kernel_size
+        self.sigma_x = config.sigma_x
 
-                        
-        c_mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR) 
+        # contour parameters
+        self.contour_color = list(map(int, config.contour_color.split(',')))
+        self.min_contour_area = config.min_contour_area
 
-        for point in points:
-            try:
-                points[point[1]][point[0]] = 255
-            except:
-                pass
+        # Canny edge parameters
+        self.canny_low_threshold = config.canny_low_threshold
+        self.canny_high_threshold = config.canny_high_threshold
 
-        # linear regression best fit line
-        points = np.array([points])
-        if len(points[0]) <= 0:
-            return frame, points[0], ([0],[0])
-        line = cv2.fitLine(points, cv2.DIST_L1, 0, 0.01, 0.01)
-        v1,v2,x1,x2 = np.float32(line)
-        alpha = width
-        p1, p2 = (int(x1-alpha*v1),int(x2-alpha*v2)), (int(x1+alpha*v1),int(x2+alpha*v2))
-        cv2.line(frame, p1, p2, self.color3, thickness=3)
+        # dimensions
+        self.HEIGHT = config.frame_length
+        self.WIDTH = config.frame_width
 
-        # calculate center point of best fit line
-        slope = v2/v1
-        x = (height//2 - x2)/slope + x1
-        cv2.circle(frame, (int(x), height // 2), 5, self.color1, -1)
+        # contours
+        self.contours = []
 
-        # reference center line and point
-        center_point = (width // 2, height // 2)
-        cv2.line(frame, center_point, (width // 2, 0), self.color2, thickness=3)
-        cv2.circle(frame, center_point, 5, self.color2, -1)
+        # center contour
+        self.center = None
+        self.center_angle = 0
 
-        # calculate angle
-        up = [0,-1]
-        down = [0,1]
-        dir = [v1,v2]
-        norm = np.linalg.norm(up) * np.linalg.norm(dir)
-        u_dp, d_dp = np.dot(up,dir)/norm, np.dot(down,dir)/norm
-        u_angle, d_angle = np.arccos(u_dp), np.arccos(d_dp)
-        angle = min(u_angle,d_angle)
-        deg = angle*180/np.pi
-        direction = "left" if u_angle > d_angle else "right"
-        sign = 1 if u_angle > d_angle else -1
+    def process_frame(self, frame, show):
+        """Uses contouring to create contours around each crop row and uses these contours to find centroid lines,
+        row vanishing point, a center contour and the angle between the center contour and vanishing point\n
+        :param frame: current frame (mat)
+        :param show: show/hide frames on screen for debugging
+        :type show: bool
+        :return: processed frame (mat), angle [-90, 90]
+        """
+        mask = self.create_binary_mask(frame)
+        mask = self.morpholoical_close(mask)
+        # Perform canny edge detection
+        # edges = cv.Canny(mask, self.canny_low_threshold, self.canny_high_threshold)
+        # blurred_edges = self.gaussian_blur(edges)
 
-        txt = "angle: " + str(np.round(deg[0], 3)) + " deg " + direction + " x offset: " + str(width//2-int(x)) + " pixels"
-        cv2.putText(frame, txt,(0,100), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 255, 255), 2, cv2.LINE_AA)
+        # current frame is a 720 x 1280 black frame
+        black_frame = np.uint8(np.zeros((720, 1280, 3)))
 
-        lines = [line]
-        points = points[0]
+        contours, contour_frame = self.get_contours(mask)
+        cv.drawContours(black_frame, contours, -1, self.contour_color, 3)
+        # fillPoly fills in the polygons in the frame
+        cv.fillPoly(black_frame, pts=contours, color=self.contour_color)
+        lines, slopes, ellipse_frame = self.ellipse_slopes(contours, black_frame)
 
-        deltas = (sign*deg, width//2 - x)
         if show:
-            # cv2.imshow('frame', frame)
-            cv2.imshow('mask', mask)
-            cv2.imshow('points', points)
-        return frame, points, deltas
-    
-    def check_end_of_row(self, frame, points):
+            Lines.draw_lines_on_frame(lines, black_frame)
 
-        height, width = frame.shape[0], frame.shape[1]
-        if len(points) <= 0:
-            end_of_row = True
-            cv2.putText(frame, "end of row detected",(width // 2 , height // 2), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 0, 0), 2, cv2.LINE_AA)
-            return frame, end_of_row
-
-        avg = np.sum(points, axis=0) / len(points)
-        cv2.circle(frame, (avg[0], avg[1]), 5, self.color1, -1) 
-
-        end_of_row = False
-        if avg[1] > height*self.end_of_row_cut_off:
-            end_of_row = True
-            cv2.putText(frame, "end of row detected",(width // 2 , height // 2), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (255, 0, 0), 2, cv2.LINE_AA)
-
-        return frame, end_of_row
-
-    
-    def process_frame(self, original_frame, show, delta=False):
-        # original_frame: BGR frame
-        # returns frame: original_frame with the lines and centroids drawn on
-
-        # increment frame counter
-        self.frame_counter += 1
-
-        # check if time to process a frame
-        if self.frame_counter % self.speed_up != 0:
-            if delta:
-                return self.last_valid_frame, True, False, (0,0)
-            else:
-                return self.last_valid_frame, True, False
-
-        # call processing functions
-        frame = self.apply_filters(original_frame)
-        frame, points, deltas = self.get_center_hough_lines(frame, show)
-
-        frame, end_of_row = self.check_end_of_row(frame, points)
-
-        # store frames
-        self.last_valid_frame = frame
-
-        if delta: #commented out False
-            return frame, False, end_of_row, deltas
+        if len(lines) == 1:
+            line = lines[0]
+            delta_y = (line[0])
+            delta_x = (line[1]-line[3])
+            angle = round(math.degrees(math.atan(delta_y/delta_x)), 2)
+            return black_frame, -angle
         else:
-            return frame, False, end_of_row
+            return black_frame, None
+
+    def create_binary_mask(self, frame):
+        """
+        :param frame: current frame
+        :return: binary mask of frame made using self.low_green and self.high_green as the HSV range
+        """
+
+        # Run averaging filter to blur the frame
+        kernel = np.ones((5, 5), np.float32) / 25
+        frame = cv.filter2D(frame, -1, kernel)
+
+        # Convert to hsv format to allow for easier colour filtering
+        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+        # Filter image and allow only shades of green to pass
+        mask = cv.inRange(hsv, self.LOW_GREEN, self.HIGH_GREEN)
+
+        return mask
+
+    def morpholoical_close(self, mask):
+        kernel = np.ones((5, 5), np.uint8)
+        closing = cv.morphologyEx(mask, cv.MORPH_CLOSE, kernel)
+
+        # im_fill = closing.copy()
+        # new_mask = np.zeros((self.HEIGHT+2, self.WIDTH+2), np.uint8)
+        # cv.floodFill(im_fill, new_mask, (0, 0), 255)
+        # im_fill = cv.bitwise_not(im_fill)
+        # closing = closing | im_fill
+
+        return closing
+
+    def get_contours(self, binary_mask):
+        """ Uses the cv.findContours to find the contours in binary_mask
+        (creates closed shapes around connected pixels)\n
+        :param binary_mask: binary mask of current frame
+        :return: list of contours, blank frame with contours filled in
+        """
+        frame = np.zeros((self.HEIGHT, self.WIDTH, 3))
+        ret, thresh = cv.threshold(binary_mask, 0, 254, 0)
+        contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+        cv.drawContours(frame, contours, -1, (0, 255, 0), 2)
+        cv.fillPoly(frame, pts=contours, color=(0, 255, 0))
+
+        return contours, frame
+
+    def ellipse_slopes(self, contours, black_frame):
+        """Draws ellipses around each contour on black_frame using cv.fitEllipse,
+        Uses cv.fitline with the list of contours to create a set of lines\n
+        :param contours: list of contours on the current frame
+        :param black_frame: black frame with contours (mat)
+        :return: lines (array), slops (array), updated frame with ellipses and lines
+        """
+        slopes = []
+        lines = []
+        self.contours = []
+
+        for cnt in contours:
+            if self.min_contour_area < cv.contourArea(cnt):
+                rect = cv.minAreaRect(cnt)
+                box = cv.boxPoints(rect)
+                box = np.int0(box)
+                black_frame = cv.drawContours(black_frame, [box], 0, (255, 255, 255), 2)
+                ellipse = cv.fitEllipse(cnt)
+                self.contours.append(ellipse)
+                cv.ellipse(black_frame, ellipse, (255, 255, 255), 2)
+                rows, cols = black_frame.shape[:2]
+                # Defines a line for the contour using the fitLine function
+                [vx, vy, x, y] = cv.fitLine(cnt, cv.DIST_L2, 0, 0.01, 0.01)
+                # calculates the slope and adds the slope to the array of slopes
+                slope = vy / vx
+                slopes.append(slope)
+                # Finds two other points on the line using the slope
+                lefty = int((-x * vy / vx) + y)
+                righty = int(((cols - x) * vy / vx) + y)
+                # black_frame = cv.line(black_frame, (cols - 1, righty), (0, lefty), (255, 255, 0), 9)
+                # Appends a line to the lines array using the (x1,y1,x2,y2) definition
+                lines.append([cols - 1, righty, 0, lefty])
+
+        return lines, slopes, black_frame
+
+    def get_center_row_coords(self):
+        """
+        :return: center coordinates of center row contour (x, y) for most recently processed frame. This is different
+        from the vanishing point.
+        """
+        return self.center[0]
+
+    def get_angle_to_minimize(self):
+        """
+        :return: angle to minimize for most recently processed frame in Radians.
+        It is the angle between the vanishing point and the center of the center row contour
+        """
+        return self.center_angle
+
+    def gaussian_blur(self, frame):
+        """Applies gaussian blurring to the frame\n
+        :param frame: current frame
+        :return: frame with gaussian blurring
+        """
+        return cv.GaussianBlur(frame, self.gauss_kernel_size, self.sigma_x)
